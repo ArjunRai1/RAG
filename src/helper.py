9fd59d9698ebb49bda9e7ba18a5202d7bed1f673
ArjunRai1 <arjunrai2214@gmail.com>
2025-03-12 12:57:45 +0530

    implementation

--- src/helper.py
+++ src/helper.py
@@ -9,7 +9,8 @@ from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
+os.environ['GOOGLE_API_KEY'] =  GOOGLE_API_KEY
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -20,17 +21,22 @@ def get_pdf_text(pdf_docs):
     return text
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=20)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
     chunks = text_splitter.split_text(text)
     return chunks
 
 def get_vector_store(text_chunks):
-    embeddings = GooglePalmEmbeddings()
+    api_key = os.getenv("GOOGLE_API_KEY")
+    if not api_key:
+        raise ValueError("GOOGLE_API_KEY is missing. Please check your environment variables.")
+
+    embeddings = GooglePalmEmbeddings(google_api_key=api_key)
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     return vector_store
+
 
 def get_conversational_chain(vector_store):
     llm = GooglePalm()
     memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
-    conversational_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory = memory)
-    return conversational_chain
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory = memory)
+    return conversation_chain
