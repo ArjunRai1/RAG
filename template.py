c3f4550d5ea5a31d886f45f2030981a0d8c200a3
ArjunRai1 <arjunrai2214@gmail.com>
2025-03-12 11:09:54 +0530

    setup and requirements

---
+++ template.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+list_of_files = [
+    f"src/__init__.py",
+    f"src/helper.py",
+    "setup.py",
+    "requirements.txt",
+    ".env",
+    "app.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir!="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created {filedir} for the file {filename}")
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as file:
+            pass
+            logging.info(f"Creating empty file: {filename}")
+    else:
+        logging.info(f"{filename} already exists")
