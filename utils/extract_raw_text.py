263aef9fe4f54c2b0db6f9f68bfd8f66f5a197c8
ArjunRai1 <arjunrai2214@gmail.com>
2025-10-17 10:32:30 +0530

    Created utility functions

---
+++ utils/extract_raw_text.py
@@ -0,0 +1,9 @@
+from PyPDF2 import PdfReader
+
+def extract_raw_text(pdf_docs):
+    raw_text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            raw_text += page.extract_text()
+    return raw_text
